6e3036071c8ff19258a6689328af0275cac12566
M.C <hanhchaumn@gmail.com>
2018-11-07 17:14:43 +0700

    Implement graphql resolvers

--- functions/firestore.js
+++ functions/firestore.js
@@ -1,59 +1,93 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
 
+class Address {
+  constructor(id, { street, country, district, city, ward, lat, lng }) {
+    this.id = id;
+    this.street = street;
+    this.country = country;
+    this.district = district;
+    this.city = city;
+    this.ward = ward;
+    this.lat = lat;
+    this.lng = lng;
+  }
+
+  getPlainObject() {
+    return {
+      street: this.street,
+      country: this.country,
+      city: this.city,
+      district: this.district,
+      ward: this.ward,
+      lat: this.lat,
+      lng: this.lng
+    };
+  }
+}
+
 exports.get = id =>
-	new Promise((resolve, reject) => {
-		db.collection('addresses')
-			.doc(id)
-			.get()
-			.then(snapshot => {
-				const data = Object.assign({}, snapshot.data(), {
-					id
-				});
-				resolve(data);
-				return snapshot;
-			})
-			.catch(console.log);
-	});
+  new Promise((resolve, reject) => {
+    db.collection("addresses")
+      .doc(id)
+      .get()
+      .then(snapshot => {
+        const data = Object.assign({}, snapshot.data(), {
+          id
+        });
+        resolve(data);
+        return snapshot;
+      })
+      .catch(console.log);
+  });
 
 exports.getAll = () =>
-	new Promise((resolve, reject) => {
-		db.collection('addresses')
-			.get()
-			.then(snapshot => {
-				let addresses = [];
-				snapshot.forEach(doc => {
-					const data = Object.assign({}, doc.data(), {
-						id: doc.id
-					});
-					addresses.push(data);
-				});
+  new Promise((resolve, reject) => {
+    db.collection("addresses")
+      .get()
+      .then(snapshot => {
+        let addresses = [];
+        snapshot.forEach(doc => {
+          const data = Object.assign({}, doc.data(), {
+            id: doc.id
+          });
+          addresses.push(data);
+        });
 
-				resolve(addresses);
-				return snapshot;
-			})
-			.catch(console.log);
-	});
+        resolve(addresses);
+        return snapshot;
+      })
+      .catch(console.log);
+  });
 
-exports.update = address =>
-	new Promise((resolve, reject) => {
-		db.collection('addresses')
-			.doc(id)
-			.set(address, {
-				merge: true
-			})
-			.then(() => resolve())
-			.catch(console.log);
-	});
+exports.insert = address =>
+  new Promise((resolve, reject) => {
+    const addr = new Address(undefined, address).getPlainObject();
+    db.collection("addresses")
+      .add(addr)
+      .then(doc => resolve(doc.id))
+      .catch(console.log);
+  });
+
+exports.update = (id, address) =>
+  new Promise((resolve, reject) => {
+    const addr = new Address(id, address).getPlainObject();
+    console.log(JSON.stringify(addr));
+    db.collection("addresses")
+      .doc(id)
+      .update(addr)
+      .then(() => resolve(id))
+      .catch(console.log);
+  });
 
 exports.delete = id =>
-	new Promise((resolve, reject) => {
-		db.collection('addresses')
-			.doc(id)
-			.delete()
-			.then(() => resolve())
-			.catch(console.log);
-	});
+  new Promise((resolve, reject) => {
+    db.collection("addresses")
+      .doc(id)
+      .delete()
+      .then(() => resolve(id))
+      .catch(console.log);
+  });
--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,30 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const cors = require('cors');
-const { buildSchema } = require('graphql');
-const firestore = require('./firestore');
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const functions = require("firebase-functions");
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
+const { buildSchema } = require("graphql");
+const firestore = require("./firestore");
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     addresses: [Address]
     address(id: String!): Address
+  }
+  type Mutation {
+    insert(input: AddressInput!): String
+    update(id: String!, input: AddressInput!): String
+    delete(id: String!): String
+  }
+  input AddressInput {
+    lat: Float
+    lng: Float
+    ward: String
+    country: String
+    city: String
+    district: String
+    street: String
   }
   type Address {
       id: String
@@ -25,27 +40,30 @@ const schema = buildSchema(`
 
 // The root provides a resolver function for each API endpoint
 const root = {
-    address: ({ id }) => firestore.get(id).then(val => val),
-    addresses: () => firestore.getAll().then(val => val)
+  address: ({ id }) => firestore.get(id).then(val => val),
+  addresses: () => firestore.getAll().then(val => val),
+  insert: ({input}) => firestore.insert(input).then(val => val),
+  update: ({id, input}) => firestore.update(id, input).then(val => val),
+  delete: ({ id }) => firestore.delete(id).then(val => val)
 };
 
 const app = express();
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(
-	'/graphql',
-	graphqlHTTP({
-		schema,
-		rootValue: root,
-		graphiql: true
-	})
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
 );
 
 const api = functions.https.onRequest(app);
 
 module.exports = {
-	api
+  api
 };
 
 // // Create and Deploy Your First Cloud Functions
